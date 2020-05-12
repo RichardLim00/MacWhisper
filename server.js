2eb680f8be45d08adcaaadc25a5c98c5b80531da
Richard Lim <richmars.lim@gmail.com>
2020-05-12 14:04:23 +0800

    Wrap Application in Server

---
+++ server.js
@@ -0,0 +1,14 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+})
+
+app.listen(PORT, () => {
+    console.log('Listening on PORT: ' + PORT)
+})
